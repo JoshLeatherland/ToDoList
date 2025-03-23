bef1370b90e9e861824b0f35107dba38b8763a20
Joshua <xjoshua2015x@gmail.com>
2025-03-23 14:09:48 +0000

    Setup StageAlias Env

--- backend/Business/Services/AwsSecretService.cs
+++ backend/Business/Services/AwsSecretService.cs
@@ -1,12 +1,15 @@
 ﻿using Amazon.SecretsManager;
 using Amazon.SecretsManager.Model;
 using Business.Services.Interfaces;
+using Microsoft.Extensions.Options;
+using Models.ViewModels;
 
 namespace Business.Services
 {
-    public class AwsSecretService(IAmazonSecretsManager secretsManager) : IAwsSecretService
+    public class AwsSecretService(IAmazonSecretsManager secretsManager, IOptions<AwsResources> awsResourcesOptions) : IAwsSecretService
     {
         private readonly IAmazonSecretsManager _secretsManager = secretsManager;
+        private readonly AwsResources _awsResources = awsResourcesOptions.Value;
 
         public async Task<string> GetSecretASync(string secretName)
         {
@@ -14,7 +17,7 @@ namespace Business.Services
             {
                 var request = new GetSecretValueRequest
                 {
-                    SecretId = secretName
+                    SecretId = GetSecretName(secretName)
                 };
 
                 var response = await _secretsManager.GetSecretValueAsync(request);
@@ -25,6 +28,14 @@ namespace Business.Services
             {
                 throw new Exception($"Unable to find secret value: {secretName}", ex);
             }
+        }
+
+        private string GetSecretName(string secretName)
+        {
+            if (string.IsNullOrEmpty(_awsResources.StageAlias))
+                return secretName;
+
+            return $"{secretName}-{_awsResources.StageAlias}";
         }
     }
 }
--- backend/Models/ViewModels/AwsResources.cs
+++ backend/Models/ViewModels/AwsResources.cs
@@ -3,5 +3,6 @@
     public class AwsResources
     {
         public string DynamoSharedTable { get; set; } = string.Empty;
+        public string StageAlias { get; set; } = string.Empty;
     }
 }
